8d72c744efbdb3011e9697a018e1a8d66cb81bdc
paulomarquess <paulinho_marques2012@hotmail.com>
2022-09-29 11:30:34 -0300

    fix: retirado bug no codigo font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   Chat,
   Copyright,
@@ -14,44 +14,39 @@ import {
 import { GlobalStyle } from "../styles";
 
 const Home = () => {
-  const [showComponent, setShowComponent] = useState(false);
   const [overflow, setOverflow] = useState("");
-  useEffect(() => {
-    setShowComponent(true);
-  }, []);
+
   return (
     <>
-      <GlobalStyle overflow={overflow} />
-      {showComponent && (
-        <>
-          <Head>
-            <title>CMSK Bodybuilding Fitness</title>
-            <link rel="icon" type="image/svg+xml" href="/cmsk.png" />
-            <link
-              ref={`https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@500;600&family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap`}
-            />
-            <meta
-              name="keywords"
-              content="cmsk,bodybuilding,saude,musculacao,fisico,condicionamento,forma,poder,corpo,musculo,online,personal,trainer,traine,treino,resultado,nutricao,nutricionista,modelagem,perder,peso,hipertrofia,muscular,aumento,condicionamento,ganho,massa,magra,bariatrica,preparador,atleta,atletas,arnold,fisiculturismo,fisiculturista,charles,mario,monica"
-            />
-            <meta name="author" content="Charles Mario" />
-            <meta
-              name="description"
-              content="Usar o conhecimento do bodybuilding para transformação dos físicos, pautado em estratégias e planejamentos completos, uma arte em esculpir corpos, de acordo com os objetivos de cada um, desde de emagrecimento, tônus, ganho de massa muscular, modelagem etc."
-            />
-            <meta name="title" content="CMSK Bodybuilding Fitness" />
-          </Head>
-          <Header />
-          <Presentation />
-          <Mission />
-          <Team setOverflow={setOverflow} />
-          <Plans />
-          <Testimonial />
-          <Footer />
-          <Chat />
-          <Copyright />
-        </>
-      )}
+      <>
+        <Head>
+          <title>CMSK Bodybuilding Fitness</title>
+          <link rel="icon" type="image/svg+xml" href="/cmsk.png" />
+          <link
+            ref={`https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@500;600&family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,300&display=swap`}
+          />
+          <meta
+            name="keywords"
+            content="cmsk,bodybuilding,saude,musculacao,fisico,condicionamento,forma,poder,corpo,musculo,online,personal,trainer,traine,treino,resultado,nutricao,nutricionista,modelagem,perder,peso,hipertrofia,muscular,aumento,condicionamento,ganho,massa,magra,bariatrica,preparador,atleta,atletas,arnold,fisiculturismo,fisiculturista,charles,mario,monica"
+          />
+          <meta name="author" content="Charles Mario" />
+          <meta
+            name="description"
+            content="Usar o conhecimento do bodybuilding para transformação dos físicos, pautado em estratégias e planejamentos completos, uma arte em esculpir corpos, de acordo com os objetivos de cada um, desde de emagrecimento, tônus, ganho de massa muscular, modelagem etc."
+          />
+          <meta name="title" content="CMSK Bodybuilding Fitness" />
+        </Head>
+        <Header />
+        <Presentation />
+        <Mission />
+        <Team setOverflow={setOverflow} />
+        <Plans />
+        <Testimonial />
+        <Footer />
+        <Chat />
+        <Copyright />
+        <GlobalStyle overflow={overflow} />
+      </>
     </>
   );
 };
